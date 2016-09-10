ca8d3cb1c9a342ccb7c622cf5378a4f835123165
Heather Fenton <hfenton@virtualhold.com>
2016-09-10 15:22:55 -0400

    Refactor strain module, erlang

--- erlang/strain/strain.erl
+++ erlang/strain/strain.erl
@@ -2,24 +2,21 @@
 -export([keep/2, discard/2]).
 
 keep(Fun, List) ->
-    keep(Fun, List, []).
+    produce_list_for(keep, Fun, List, []).
 discard(Fun, List) ->
-    discard(Fun, List, List).
+    produce_list_for(discard, Fun, List, List).
 
-keep(_Fun, [], Acc) ->
+produce_list_for(_Keep_Discard, _Fun, [], Acc) ->
     Acc;
-keep(Fun, [H|T], Acc) ->
-    keep(Fun, T, add_to_list(Fun(H), H, Acc)).
+produce_list_for(keep, Fun, [H|T], Acc) ->
+    produce_list_for(keep, Fun, T, add_to_list(Fun(H), H, Acc));
+produce_list_for(discard, Fun, [H|T], Acc) ->
+    produce_list_for(discard, Fun, T, remove_from_list(Fun(H), H, Acc)).
 
 add_to_list(true, ItemToAdd, Acc) ->
     Acc ++ [ItemToAdd];
 add_to_list(false, _, Acc) ->
     Acc.
-
-discard(_Fun, [], Acc) ->
-    Acc;
-discard(Fun, [H|T], Acc) ->
-    discard(Fun, T, remove_from_list(Fun(H), H, Acc)).
 
 remove_from_list(true, Item, Acc) ->
     Acc -- [Item];
