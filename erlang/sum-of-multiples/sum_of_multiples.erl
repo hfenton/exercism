4affcd71d50be0d6a1e530018df2d896fc58b2c9
Heather Fenton <hfenton@virtualhold.com>
2016-09-27 14:04:45 -0400

    [erlang] - sum-of-multiples - refactor to use lists:seq

--- erlang/sum-of-multiples/sum_of_multiples.erl
+++ erlang/sum-of-multiples/sum_of_multiples.erl
@@ -7,9 +7,5 @@ sumOfMultiples(SomeList, MaxValue) ->
 sum_list_to_max_value([], _MaxValue, Acc) ->
     lists:sum(lists:usort(Acc));
 sum_list_to_max_value([H|T], MaxValue, Acc) ->
-    sum_list_to_max_value(T, MaxValue, get_multiples(H, H, MaxValue, Acc)).
+    sum_list_to_max_value(T, MaxValue, Acc ++ lists:seq(H, MaxValue - 1, H)).
 
-get_multiples(NumberToCheck, OriginalNumber, MaxValue, Acc) when NumberToCheck < MaxValue ->
-    get_multiples(NumberToCheck + OriginalNumber, OriginalNumber, MaxValue, Acc ++ [NumberToCheck]);
-get_multiples(_NumberToCheck, _OriginalNumber, _MaxValue, Acc) ->
-    Acc.
